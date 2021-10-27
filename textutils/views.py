c6b88fb79e6b66ccd8af1cf6133b800cd52baca1
Aizaz Haider <aizazisonline@gmail.com>
2021-10-27 15:12:57 +0500

    Added Upper, Lower, Capitalize etc

--- textutils/views.py
+++ textutils/views.py
@@ -8,24 +8,73 @@ def indexFunc(request):
 
 
 def analyze(request):
+    # Getting Text
     inputText = request.GET.get('textareaNameHTML', 'default')
-    print(inputText)
+    # Getting On / Off Values
     remText = request.GET.get('removePuncHTML', 'off')
-    print(remText)
+    upperText = request.GET.get('upperHTML', 'off')
+    lowerText = request.GET.get('lowerHTML', 'off')
+    capitalizeText = request.GET.get('capitalizeHTML', 'off')
+    newLineText = request.GET.get('newLineHTML', 'off')
+    extraSpaceText = request.GET.get('extraSpaceHTML', 'off')
+
+    # Selections Statement Starts Here
     if remText == 'on':
         totalpunctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed_var = ''
         for i in inputText:
             if i not in totalpunctuations:
                 analyzed_var = analyzed_var + i
-
-        params = {'analyzed_text': analyzed_var, 'purpose': 'Remove Punctuations', }
+        params = {'analyzed_text': analyzed_var}
         return render(request, 'analyze.html', params)
-
+    elif upperText == 'on':
+        analyzedcaps = ""
+        for i in inputText:
+            analyzedcaps = analyzedcaps + i.upper()
+        params = {'yourUpperCase_text': analyzedcaps}
+        return render(request, 'analyze.html', params)
+    elif lowerText == 'on':
+        analyzedlower = ""
+        for i in inputText:
+            analyzedlower = analyzedlower + i.lower()
+        params = {'yourLowerCase_text': analyzedlower}
+        return render(request, 'analyze.html', params)
+    elif capitalizeText == 'on':
+        analyzedcapitalize = str(inputText).capitalize()
+        # for i in inputText:
+        #     analyzedcapitalize = analyzedcapitalize + i.capitalize()
+        params = {'yourCapitalize_text': analyzedcapitalize}
+        return render(request, 'analyze.html', params)
+    elif newLineText == 'on':
+        analyzedNewLine= ""
+        for i in inputText:
+            if i != "\n":
+                analyzedNewLine = analyzedNewLine + i
+        params = {'yourNewLine_text': analyzedNewLine}
+        return render(request, 'analyze.html', params)
+    elif extraSpaceText == 'on':
+        analyzedExtraSpace= ""
+        for i in inputText:
+            if i != "  ":
+                analyzedExtraSpace = analyzedExtraSpace + i
+        params = {'yourExtraSpace_text': analyzedExtraSpace}
+        return render(request, 'analyze.html', params)
     else:
         return HttpResponse('Something Went Wrong (Aizaz)')
 
 
+
+
+
+    # elif upperText == 'on' and remText == 'on':
+    #     totalpunctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    #     analyzed_var = ''
+    #     for i in inputText:
+    #         if i not in totalpunctuations:
+    #             analyzed_var = analyzed_var + i
+    #     doingUpperCaseVar = str(inputText).upper()
+    #     params = {'analyzed_text': analyzed_var, 'yourUpperCase_text': doingUpperCaseVar}
+    #     return render(request, 'analyze.html', params)
 
 
 
